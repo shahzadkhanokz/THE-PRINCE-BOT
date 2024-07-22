67d0c1fb90e54a45e20d10975d9255f166b5eb4b
Silentkiller <153942382+shahzadkhanokz@users.noreply.github.com>
2024-07-22 14:06:51 +0500

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ for (let i = 0; i < ownerlist.length; i += 2) {
 
 
 //💌global.pairingNumber = "" //put your bot number here💌
-global.mods = ['923042205427'] 
+global.mods = ['92459646503'] 
 global.prems = ['']
 global.allowed = ['']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
